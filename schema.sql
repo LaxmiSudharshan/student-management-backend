47f18e943a76e80b0bcdc0c1132ee8dd53765263
LaxmiSudharshan <124552161+LaxmiSudharshan@users.noreply.github.com>
2025-05-17 08:01:48 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE student_db;
+USE student_db;
+
+CREATE TABLE students (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  roll_no VARCHAR(50),
+  branch VARCHAR(100),
+  email VARCHAR(100)
+);
